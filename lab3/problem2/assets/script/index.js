13b70384b187d5d3a10a3372dea7ef551d6d7e4d
jeisper <71713717+jeisper@users.noreply.github.com>
2022-11-24 14:37:31 +0000

    second lab code

---
+++ lab3/problem2/assets/script/index.js
@@ -0,0 +1,45 @@
+const { fromEvent, interval, merge, EMPTY } = rxjs;
+const { take } = rxjs.operators;
+
+const hour = document.querySelector("#hour");
+const min = document.querySelector("#min");
+const result = document.querySelector("#result");
+const start = document.querySelector("#start");
+
+const getStartValue = () => {
+  let hrs = hour.value * 60 * 60;
+  let mns = min.value * 60;
+  return hrs + mns;
+};
+
+const getTimeFromSeconds = (totalSeconds) => {
+  const hour = Math.floor(totalSeconds / (60 * 60));
+  const min = Math.floor(totalSeconds / 60) % 60;
+
+  return { hour, min };
+};
+
+const startObservable = fromEvent(start, "click");
+
+const startTime = getTimeFromSeconds(getStartValue());
+result.innerHTML =
+  startTime.hour +
+  "hours " +
+  (startTime.min == 60 ? 0 : startTime.min) +
+  " mins ";
+
+let timerSubscription = null;
+
+const subscription = startObservable.subscribe(() => {
+  if (timerSubscription) timerSubscription.unsubscribe();
+
+  const startValue = getStartValue();
+  const timer = interval(1000).pipe(take(getStartValue()));
+
+  timerSubscription = timer.subscribe((x) => {
+    const time = getTimeFromSeconds(startValue - x);
+
+    result.innerHTML =
+      time.hour + "hours " + (time.min == 60 ? 0 : time.min) + " mins ";
+  });
+});
