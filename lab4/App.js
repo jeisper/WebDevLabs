ce2ad3b58462cebbd182d5abdc15493eff00cea3
jeisper <71713717+jeisper@users.noreply.github.com>
2022-12-14 23:51:52 +0000

    lab 4 upload

---
+++ lab4/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import NoteForm from "./components/NoteForm";
+import NoteList from "./components/NoteList";
+
+function App() {
+  return (
+    <div className="note-app">
+      <NoteList />
+    </div>
+  );
+}
+
+export default App;
